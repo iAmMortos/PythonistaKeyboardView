73ec204e8c5077438b2db680a0a8cf927fe309a7
Mortos Der Soulstealer <iAmMortos@users.noreply.github.com>
2019-10-09 12:21:31 -0600

    fixed escaped backslash key character

--- keyboard_view.py
+++ keyboard_view.py
@@ -9,14 +9,10 @@ class KeyboardView (ui.View):
 	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		alpha_keys = \
-		['qwertyuiop','asdfghjkl','zxcvbnm']
-		alpha_alts = \
-		['QWERTYUIOP','ASDFGHJKL','ZXCVBNM']
-		special_keys = \
-		['1234567890','-/:;()$&@"','.,?!\'']
-		special_alts = \
-		['[]{}#%^*+=','_\|~<>€£¥•','.,?!\'']
+		alpha_keys = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+		alpha_alts = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM']
+		special_keys = ['1234567890', '-/:;()$&@"', '.,?!\'']
+		special_alts = ['[]{}#%^*+=', '_\\|~<>€£¥•', '.,?!\'']
 		
 		self.panels = [KeyboardPanel(alpha_keys, alpha_alts), KeyboardPanel(special_keys, special_alts)]
 		
